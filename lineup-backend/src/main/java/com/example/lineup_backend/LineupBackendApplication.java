d80c64e8440277953f5ef0617b60a27743692ac3
akashporia-ucl <ucabpor@ucl.ac.uk>
2024-11-22 21:33:14 +0000

    First commit

---
+++ lineup-backend/src/main/java/com/example/lineup_backend/LineupBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.lineup_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LineupBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LineupBackendApplication.class, args);
+	}
+
+}
